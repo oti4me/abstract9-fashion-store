d176bb671b0e747b7dc3e6fed384765ccd6a5a42
Henry Otighe <henryotighe@Andelas-MacBook-Pro.local>
2020-03-11 01:10:58 +0100

    make the user model polymorphic

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -16,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'userable_id', 'userable_type',
     ];
 
     /**
@@ -36,4 +37,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return MorphTo
+     */
+    public function userable()
+    {
+        return $this->morphTo();
+    }
 }
