5251e0dd57aa2ba018ff9d0706c67db065a9f811
Henry Otighe <henryotighe@Andelas-MacBook-Pro.local>
2020-03-13 22:00:36 +0100

    fix failing test

--- tests/Feature/Controllers/AdminControllerTest.php
+++ tests/Feature/Controllers/AdminControllerTest.php
@@ -5,10 +5,13 @@ namespace Tests\Feature\Controllers;
 
 
 use App\Models\Admin;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AdminControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_can_view_admin_dashboard()
     {
         $this->actingAs(factory(Admin::class)->create(), 'admin')
